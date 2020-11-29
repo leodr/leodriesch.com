ddb280d9134613cd629157c8f8e72bb9c15f5a91
Leo Driesch <leo.driesch@gmail.com>
2020-11-29 22:32:06 +0100

    :sparkles: Add footer content

--- src/layouts/StandardLayout.js
+++ src/layouts/StandardLayout.js
@@ -1,5 +1,6 @@
 import { Transition } from "@headlessui/react"
 import { Logo } from "components/Logo"
+import { NewsletterForm } from "components/NewsletterForm"
 import Link from "next/link"
 import { useState } from "react"
 
@@ -284,161 +285,69 @@ export function StandardLayout({
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                     <div className="xl:grid xl:grid-cols-3 xl:gap-8">
                         <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-                            <div className="md:grid md:grid-cols-2 md:gap-8">
-                                <div>
+                            <div className="col-span-2 flex space-x-4 sm:col-span-1 sm:grid sm:grid-cols-2 sm:gap-8">
+                                <div className="flex-1">
                                     <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                                        Solutions
+                                        Sitemap
                                     </h3>
                                     <ul className="mt-4 space-y-4">
                                         <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Marketing
-                                            </a>
+                                            <Link href="/">
+                                                <a className="text-base text-gray-300 hover:text-white">
+                                                    Home
+                                                </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Analytics
-                                            </a>
+                                            <Link href="/blog">
+                                                <a className="text-base text-gray-300 hover:text-white">
+                                                    Projects
+                                                </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Commerce
-                                            </a>
+                                            <Link href="/blog">
+                                                <a className="text-base text-gray-300 hover:text-white">
+                                                    Blog
+                                                </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Insights
-                                            </a>
+                                            <Link href="/contact">
+                                                <a className="text-base text-gray-300 hover:text-white">
+                                                    Contact
+                                                </a>
+                                            </Link>
                                         </li>
                                     </ul>
                                 </div>
-                                <div className="mt-12 md:mt-0">
+                                <div className="flex-1">
                                     <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                                        Support
+                                        OSS
                                     </h3>
                                     <ul className="mt-4 space-y-4">
                                         <li>
                                             <a
-                                                href="#"
+                                                href="https://github.com/leodr/leodriesch.com"
                                                 className="text-base text-gray-300 hover:text-white"
                                             >
-                                                Pricing
+                                                leodriesch.com
                                             </a>
                                         </li>
                                         <li>
                                             <a
-                                                href="#"
+                                                href="https://marketplace.visualstudio.com/items?itemName=leodriesch.paste-html-as-jsx"
                                                 className="text-base text-gray-300 hover:text-white"
                                             >
-                                                Documentation
+                                                Paste HTML as JSX
                                             </a>
                                         </li>
                                         <li>
                                             <a
-                                                href="#"
+                                                href="https://github.com/leodr/generate-emoji-list"
                                                 className="text-base text-gray-300 hover:text-white"
                                             >
-                                                Guides
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                API Status
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="md:grid md:grid-cols-2 md:gap-8">
-                                <div>
-                                    <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                                        Company
-                                    </h3>
-                                    <ul className="mt-4 space-y-4">
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                About
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Blog
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Jobs
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Press
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Partners
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="mt-12 md:mt-0">
-                                    <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                                        Legal
-                                    </h3>
-                                    <ul className="mt-4 space-y-4">
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Claim
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Privacy
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                href="#"
-                                                className="text-base text-gray-300 hover:text-white"
-                                            >
-                                                Terms
+                                                generate-emoji-list
                                             </a>
                                         </li>
                                     </ul>
@@ -511,6 +420,24 @@ export function StandardLayout({
                                         fillRule="evenodd"
                                         d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                                         clipRule="evenodd"
+                                    />
+                                </svg>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/leodriesch/"
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                className="text-gray-400 hover:text-gray-300"
+                            >
+                                <span className="sr-only">LinkedIn</span>
+                                <svg
+                                    className="h-6 w-6"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 448 512"
+                                >
+                                    <path
+                                        fill="currentColor"
+                                        d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
                                     />
                                 </svg>
                             </a>
