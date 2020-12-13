b38b54cb6f78eb1b0e39171ca85e746870988d50
Leo Driesch <leo.driesch@gmail.com>
2020-12-13 23:58:57 +0100

    :mag: Add basic SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
+import { DefaultSeo } from "next-seo"
+import "tailwindcss/tailwind.css"
+import "../styles/blog.css"
 import "../styles/globals.css"
 
 export default function CustomApp({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page)
 
-    return getLayout(<Component {...pageProps}></Component>)
+    return getLayout(
+        <>
+            <DefaultSeo
+                openGraph={{
+                    type: "website",
+                    locale: "en_US",
+                    url: "https://leodriesch.com/",
+                    site_name: "Leo Driesch's Website",
+                }}
+                twitter={{
+                    handle: "@leodriesch",
+                    site: "@leodriesch",
+                    cardType: "summary_large_image",
+                }}
+            />
+            <Component {...pageProps}></Component>
+        </>
+    )
 }
