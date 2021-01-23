7b780451ce3777f61b6e3e5e744a14edc094ec41
Leo Driesch <leo.driesch@gmail.com>
2021-01-24 00:28:45 +0100

    :bug: Fix style issue with loading spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,11 @@
 export function Spinner(props) {
     return (
-        <>
-            <svg
-                viewBox="0 0 100 100"
-                xmlns="http://www.w3.org/2000/svg"
-                {...props}
-            >
-                <circle cx={50} cy={50} r={45} />
-            </svg>
+        <svg
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <circle cx={50} cy={50} r={45} />
             <style jsx>{`
                 svg {
                     animation: 2s linear infinite svg-animation;
@@ -54,6 +52,6 @@ export function Spinner(props) {
                     }
                 }
             `}</style>
-        </>
+        </svg>
     )
 }
