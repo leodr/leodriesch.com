21fdcb4835625660beef08f0ad12f00507a51cd6
Leo Driesch <leo.driesch@gmail.com>
2021-01-24 01:09:16 +0100

    :bento: Add favicon and app images

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { DefaultSeo } from "next-seo"
+import Head from "next/head"
 import "tailwindcss/tailwind.css"
 import "../styles/blog.css"
 import "../styles/globals.css"
@@ -8,6 +9,24 @@ export default function CustomApp({ Component, pageProps }) {
 
     return getLayout(
         <>
+            <Head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link rel="icon" type="image/png" href="/favicon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/safari-pinned-tab.svg"
+                    color="#5bbad5"
+                />
+                <meta name="apple-mobile-web-app-title" content="Leo Driesch" />
+                <meta name="application-name" content="Leo Driesch" />
+                <meta name="msapplication-TileColor" content="#f9fafb" />
+                <meta name="theme-color" content="#f9fafb" />
+            </Head>
             <DefaultSeo
                 openGraph={{
                     type: "website",
