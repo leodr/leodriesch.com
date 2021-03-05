393a2445a197d7ef4b5669cd6f782b3a60466c2d
Leo Driesch <leo.driesch@gmail.com>
2021-03-05 20:23:52 +0100

    :recycle: Extract components out of standard layout

--- src/layouts/BlogLayout.js
+++ src/layouts/BlogLayout.js
@@ -1,10 +1,24 @@
+import { format } from "date-fns"
 import { StandardLayout } from "./StandardLayout"
 
 export function BlogLayout({ children, meta }) {
     return (
-        <StandardLayout headline={meta.title}>
+        <StandardLayout
+            headline={
+                <>
+                    <span className="inline-block text-xl mb-2 tracking-normal font-medium text-gray-400">
+                        {format(
+                            new Date(meta.publishDate),
+                            "EEEE, MMMM d, yyyy"
+                        )}
+                    </span>
+                    <br />
+                    <span className="max-w-4xl inline-block">{meta.title}</span>
+                </>
+            }
+        >
             <div className="bg-white">
-                <article className="prose prose-indigo sm:prose-lg mx-auto py-12 px-3">
+                <article className="prose prose-indigo sm:prose-lg mx-auto py-12 sm:py-24 px-3">
                     {children}
                 </article>
             </div>
