3553cf97c4b5255e859732253768b3b5e02897fe
Leo Driesch <leo.driesch@gmail.com>
2021-05-16 22:40:36 +0200

    :lipstick: Fix color issue on mobile

--- components/BlogGrid.tsx
+++ components/BlogGrid.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function BlogGrid({ children }: Props) {
     return (
-        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <div className="pt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
             {children}
         </div>
     )
