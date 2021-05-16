a3efb64fdb6c56e060d20614c90bc22541002c54
Leo Driesch <leo.driesch@gmail.com>
2021-05-16 15:40:41 +0200

    :memo: Shorten URL in comment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
+/**
+ * MDX configuration inspired by Tailwind blog, see https://git.io/JslDe
+ */
+
 const createMdxPlugin = require("@next/mdx")
 const rehypePrism = require("@mapbox/rehype-prism")
 const withImages = require("next-images")
 const tokenColorizer = require("./token-colorizer")
-
-/**
- * MDX configuration inspired by Tailwind blog, see
- * https://github.com/tailwindlabs/blog.tailwindcss.com/blob/53170d9efa6439bafc2e15dbf80dc72a5d2a4b4f/next.config.js
- */
 
 const withMDX = createMdxPlugin({
     options: {
